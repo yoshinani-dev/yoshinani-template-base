453b52bb334267b4f7376f456a3f3cb1ee0a446f
FukeKazki <kazkichi0906@gmail.com>
2026-05-03 22:33:08 +0900

    feat: storybookの導入

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -3,7 +3,13 @@ import next from "@yoshinani/style-guide/eslint/next"
 const eslintConfig = [
   ...next,
   {
-    ignores: ["**/*.config.mjs", "node_modules/", "dist/", ".next/"],
+    ignores: [
+      "**/*.config.mjs",
+      "node_modules/",
+      "dist/",
+      ".next/",
+      "coverage/",
+    ],
   },
   {
     rules: {
